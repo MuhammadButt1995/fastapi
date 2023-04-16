249e6e23f62d98381ad2a718202366cb7a0dd5c8
Muhammad_Butt <Muhammadmbutt1995@gmail.com>
2023-04-15 21:38:37 -0400

    inti

--- tools/Domain_Connection_Tool/domain_connection_tool.py
+++ tools/Domain_Connection_Tool/domain_connection_tool.py
@@ -1,7 +1,7 @@
 import platform
 import subprocess
 import re
-from tools.base_tool import BaseTool
+from tools.base_tool.base_tool import BaseTool
 
 
 class DomainConnectionTool(BaseTool):
@@ -9,17 +9,19 @@ class DomainConnectionTool(BaseTool):
         super().__init__('DomainConnectionTool', 'Check if connected to ZPA or VPN', 'domain_connection_tool.png')
 
     @staticmethod
-    def check_zscaler_process(system):
+    def check_zscaler_connection(system):
         zscaler_process_pattern = re.compile(r'Zscaler')
 
         if system == 'Windows':
-            process_output = subprocess.check_output('tasklist', shell=True, text=True)
+            process_output = subprocess.check_output('tasklist /SVC /FO CSV', shell=True, text=True)
+            netstat_output = subprocess.check_output('netstat -b -n', shell=True, text=True)
         elif system == 'Darwin':
             process_output = subprocess.check_output('ps aux', shell=True, text=True)
+            netstat_output = subprocess.check_output('netstat -n -p TCP', shell=True, text=True)
         else:
             return False
 
-        if zscaler_process_pattern.search(process_output):
+        if zscaler_process_pattern.search(process_output) and zscaler_process_pattern.search(netstat_output):
             return True
 
         return False
@@ -30,6 +32,6 @@ class DomainConnectionTool(BaseTool):
         connection_status = {"zscaler_authenticated": False}
 
         if system == "Windows" or system == "Darwin":
-            connection_status["zscaler_authenticated"] = DomainConnectionTool.check_zscaler_process(system)
+            connection_status["zscaler_authenticated"] = DomainConnectionTool.check_zscaler_connection(system)
 
         return connection_status
