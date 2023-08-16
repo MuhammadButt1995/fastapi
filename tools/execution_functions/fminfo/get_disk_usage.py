2615f8de926e3f408d3837a03e1a8bc62af8ee8a
Muhammad_Butt <Muhammadmbutt1995@gmail.com>
2023-08-16 11:59:43 -0400

    init

--- tools/execution_functions/fminfo/get_disk_usage.py
+++ tools/execution_functions/fminfo/get_disk_usage.py
@@ -1,82 +1,21 @@
-import platform
-import subprocess
 import psutil
-from typing import Dict, Any
-
-async def get_disk_usage() -> Dict[str, Any]:
-    system = platform.system()
-
-    if system == "Windows":
-        return fetch_disk_usage_windows()
-    elif system == "Darwin":
-        return fetch_disk_usage_macos()
-    else:
-        raise Exception("Unsupported OS")
+from typing import Any
 
 
-def fetch_disk_usage_windows() -> Dict[str, Any]:
-    partition_info = psutil.disk_usage('C:\\')
-    total_disk_size_gb = partition_info.total / (1024**3)
-    current_disk_usage_gb = partition_info.used / (1024**3)
-    remaining_space_gb = partition_info.free / (1024**3)
-
-    return create_disk_usage_response(total_disk_size_gb, current_disk_usage_gb, remaining_space_gb)
-
-
-def fetch_disk_usage_macos() -> Dict[str, Any]:
-    cmd = ["df", "-h", "/"]
-    try:
-        stdout = subprocess.check_output(cmd)
-        lines = stdout.decode("utf-8").splitlines()
-
-        if len(lines) < 2:
-            raise Exception("Unexpected output format from `df` command")
-
-        headers = lines[0].split()
-        values = lines[1].split()
-
-        if "Used" in headers and "Size" in headers:
-            size_index = headers.index("Size")
-            used_index = headers.index("Used")
-
-            total_disk_size_gb = convert_to_gb(values[size_index])
-            current_disk_usage_gb = convert_to_gb(values[used_index])
-            remaining_space_gb = total_disk_size_gb - current_disk_usage_gb
-        else:
-            raise Exception("Unexpected column headers in `df` command output")
-
-        return create_disk_usage_response(total_disk_size_gb, current_disk_usage_gb, remaining_space_gb)
-    
-    except subprocess.CalledProcessError as e:
-        return {
-            "error": f"An error occurred while fetching disk usage for macOS: {str(e)}"
-        }
-
-
-def convert_to_gb(size_str: str) -> float:
-    """Convert sizes from string with possible 'G', 'M', 'K' suffixes to float GB."""
-    size_str = size_str.upper().rstrip('I')  # Remove 'I' suffix if present
-
-    conversion_factors = {
-        'G': 1,
-        'M': 1/1024,
-        'K': 1/(1024**2),
-    }
-
-    for suffix, factor in conversion_factors.items():
-        if size_str.endswith(suffix):
-            return float(size_str.rstrip(suffix)) * factor
-
-    return float(size_str) / (1024**3)  # assume bytes if no suffix
-
-
-def create_disk_usage_response(total_disk_size_gb: float, current_disk_usage_gb: float, remaining_space_gb: float) -> Dict[str, Any]:
+async def get_disk_usage(**params: Any):
+    total_disk_size_gb = psutil.disk_usage("/").total / (1024**3)
+    current_disk_usage_gb = psutil.disk_usage("/").used / (1024**3)
+    remaining_space_gb = total_disk_size_gb - current_disk_usage_gb
     remaining_space_percent = (remaining_space_gb / total_disk_size_gb) * 100
-    disk_utilization_health = determine_disk_health(remaining_space_percent)
-    
-    description = (f"You are using {current_disk_usage_gb:.2f} GB out of {total_disk_size_gb:.2f} GB. "
-                   f"You have {remaining_space_gb:.2f} GB ({remaining_space_percent:.2f}%) of disk space remaining.")
-    
+
+    if remaining_space_percent > 25:
+        disk_utilization_health = "healthy"
+    elif 15 < remaining_space_percent <= 25:
+        disk_utilization_health = "at risk"
+    else:
+        disk_utilization_health = "unhealthy"
+
+    description = f"You are using {current_disk_usage_gb:.2f} GB out of {total_disk_size_gb:.2f} GB. You have {remaining_space_gb:.2f} GB ({remaining_space_percent:.2f}%) of disk space remaining."
     rating = {
         "healthy": "ok",
         "at risk": "warn",
@@ -91,12 +30,3 @@ def create_disk_usage_response(total_disk_size_gb: float, current_disk_usage_gb:
         "description": description,
         "rating": rating,
     }
-
-
-def determine_disk_health(remaining_space_percent: float) -> str:
-    if remaining_space_percent > 25:
-        return "healthy"
-    elif 15 < remaining_space_percent <= 25:
-        return "at risk"
-    else:
-        return "unhealthy"
--- tools/execution_functions/fminfo/get_network_adapters.py
+++ tools/execution_functions/fminfo/get_network_adapters.py
@@ -1,24 +1,41 @@
 import psutil
 import socket
-from typing import Any
+import re
+from typing import Any, Dict
 
 
-async def get_network_adapters(**params: Any):
+# Dictionary to provide intuitive meanings for both Mac and Windows interfaces
+interface_patterns = {
+    r"lo[0-9]*$": "Local Loopback",
+    r"en[0-9]*$": "Primary Network Interface",
+    r"utun[0-9]*$": "VPN/Tunnel Interface",
+    r"Ethernet": "Wired Connection",
+    r"Wi-Fi": "Wireless Connection",
+    r"Loopback Pseudo-Interface [0-9]*$": "Local Loopback",
+    r"vEthernet \(.*\)$": "Virtual Ethernet"
+}
+
+
+def get_intuitive_interface_name(interface: str) -> str:
+    """Returns a user-friendly name for the network interface."""
+    for pattern, description in interface_patterns.items():
+        if re.match(pattern, interface):
+            return f"{interface} ({description})"
+    return interface
+
+
+async def get_network_adapters(**params: Any) -> Dict[str, Dict[str, str]]:
     try:
         active_adapters = {}
-        inactive_adapters = {}
 
         for interface, addrs in psutil.net_if_addrs().items():
             for addr in addrs:
-                if addr.family == socket.AF_INET:
-                    if psutil.net_if_stats()[interface].isup:
-                        active_adapters[interface] = addr.address
-                    else:
-                        inactive_adapters[interface] = addr.address
+                if addr.family == socket.AF_INET and psutil.net_if_stats()[interface].isup:
+                    label = get_intuitive_interface_name(interface)
+                    active_adapters[label] = addr.address
 
         return {
-            "active_adapters": active_adapters,
-            "inactive_adapters": inactive_adapters,
+            "active_adapters": active_adapters
         }
     except Exception as e:
-        print(e)
+        return {"error": str(e)}
