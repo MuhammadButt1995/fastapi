133009aba0771db44a7c99eeb5f9e5c338a5fe2b
Muhammad_Butt <Muhammadmbutt1995@gmail.com>
2023-08-16 13:26:10 -0400

    init

--- tools/execution_functions/fminfo/get_battery_health.py
+++ tools/execution_functions/fminfo/get_battery_health.py
@@ -3,47 +3,31 @@ import platform
 import psutil
 import subprocess
 from bs4 import BeautifulSoup
-from typing import Any
+from typing import Any, Dict, Tuple, Union
+
+# --------------------- UTILITY FUNCTIONS --------------------- #
 
 
-# Function to get system and architecture
-def get_system_architecture():
+def get_system_architecture() -> Tuple[str, str]:
+    """Return the system and architecture."""
     system = platform.system()
     architecture = platform.machine()
     return system, architecture
 
 
-# Function to check if system is Windows
-def is_windows(system):
-    return system == "Windows"
-
-
-# Function to check if system is MacOS
-def is_mac(system):
-    return system == "Darwin"
-
-
-# Function to check if architecture is ARM-based Mac
-def is_mac_arm(architecture):
-    return "arm" in architecture.lower()
-
-
-# Function to check if architecture is Intel-based Mac
-def is_mac_intel(architecture):
-    return "x86" in architecture.lower()
-
-
-# Function to get current battery metrics
-def get_battery_metrics():
+def get_battery_metrics() -> Tuple[str, bool]:
+    """Return current battery metrics: percentage and plugged state."""
     battery = psutil.sensors_battery()
-    plugged = battery.power_plugged
     percent = str(battery.percent)
-    plugged = True if plugged else False
+    plugged = battery.power_plugged
     return percent, plugged
 
 
-# Function to get battery health for Windows systems
-def get_windows_battery_health():
+# ----------------- WINDOWS SPECIFIC FUNCTIONS ----------------- #
+
+
+def get_windows_battery_health() -> Tuple[int, str]:
+    """Return battery health metrics for Windows systems."""
     dir_path = r"C:\Temp"
     file_name = "batteryreport.html"
     full_path = os.path.join(dir_path, file_name)
@@ -58,10 +42,9 @@ def get_windows_battery_health():
         with open(full_path, "r") as file:
             content = file.read()
     except Exception as e:
-        print(f"File Error: {e}")
-        return None, None
+        raise RuntimeError(f"Error reading battery report file: {e}")
 
-    # Parse battery report
+    # Parse battery report using BeautifulSoup
     soup = BeautifulSoup(content, "html.parser")
     table = soup.findAll("table")[1]
     arr = [
@@ -86,68 +69,78 @@ def get_windows_battery_health():
     return battery_health, battery_health_status
 
 
-# Function to get battery health for Mac systems
-def get_mac_battery_health(architecture):
+# ----------------- MACOS SPECIFIC FUNCTIONS ----------------- #
+
+
+def is_mac_intel(architecture: str) -> bool:
+    """Check if architecture is Intel-based Mac."""
+    return "x86" in architecture.lower()
+
+
+def is_mac_arm(architecture: str) -> bool:
+    """Check if architecture is ARM-based Mac."""
+    return "arm" in architecture.lower()
+
+
+def get_mac_battery_health(architecture: str) -> Tuple[Union[int, str], str]:
+    """Return battery health metrics for Mac systems."""
     battery_condition_cmd = subprocess.check_output(
         "system_profiler SPPowerDataType | grep Condition", shell=True
     )
     battery_condition = battery_condition_cmd.decode().split(":")[1].strip()
-
     battery_health_status = "healthy" if battery_condition == "Normal" else "unhealthy"
 
     # Different commands for Intel and ARM based Macs
-    if architecture == "macint":
+    if is_mac_intel(architecture):
         max_cap_command = subprocess.check_output(
-            "ioreg -l -w0 | grep MaxCapacity | sed - E 's/.*\"MaxCapacity\" *= *([0-9]+).*/\1/'",
-            shell=True,
+            "ioreg -l | grep MaxCapacity | awk '{print $5}'", shell=True
         )
-        max_cap = int(max_cap_command.decode().split(" = ")[1][:4])
+        max_cap = int(max_cap_command.decode().strip())
 
         design_cap_cmd = subprocess.check_output(
-            "ioreg -l -w0 | grep DesignCapacity | tail -n 1", shell=True
+            "ioreg -l | grep DesignCapacity | awk '{print $5}'", shell=True
         )
-        design_cap = int(design_cap_cmd.decode().split(" = ")[1][:4])
+        design_cap = int(design_cap_cmd.decode().strip())
 
         battery_health = round(max_cap / design_cap, 2) * 100
-    elif architecture == "macarm":
+    elif is_mac_arm(architecture):
         battery_health_cmd = subprocess.check_output(
             'system_profiler SPPowerDataType | grep "Capacity"', shell=True
         )
-        battery_health = battery_health_cmd.decode().split(":")[1].strip()
+        battery_health = int(battery_health_cmd.decode().split(":")[1].strip())
 
     return battery_health, battery_health_status
 
 
-# Main function to get battery health
-async def get_battery_health(**params: Any):
+# --------------------- MAIN FUNCTION --------------------- #
+
+
+async def get_battery_health(**params: Any) -> Dict[str, Union[str, bool]]:
+    """Main function to fetch and return battery health details."""
     try:
         system, architecture = get_system_architecture()
         battery_percent, is_plugged = get_battery_metrics()
 
-        # Different checks for Windows and Mac
-        if is_windows(system):
+        if system == "Windows":
             battery_health_percent, battery_health_status = get_windows_battery_health()
-        elif is_mac(system):
-            if is_mac_intel(architecture):
-                battery_health_percent, battery_health_status = get_mac_battery_health(
-                    "macint"
-                )
-            elif is_mac_arm(architecture):
-                battery_health_percent, battery_health_status = get_mac_battery_health(
-                    "macarm"
-                )
-
-        if battery_health_status == "healthy":
-            description = "Your battery is healthy, which means it's functioning well."
+        elif system == "Darwin":  # MacOS
+            battery_health_percent, battery_health_status = get_mac_battery_health(
+                architecture
+            )
         else:
-            description = "Your battery is unhealthy. This may affect your system's performance. Please contact tech support for assistance."
+            raise ValueError("Unsupported OS")
+
+        description = (
+            "Your battery is healthy, which means it's functioning well."
+            if battery_health_status == "healthy"
+            else "Your battery is unhealthy. This may affect your system's performance. Please contact tech support for assistance."
+        )
 
         rating = {
             "healthy": "ok",
             "unhealthy": "error",
         }[battery_health_status]
 
-        # Return battery health information
         return {
             "battery_health_status": battery_health_status.upper(),
             "battery_health_percentage": str(int(battery_health_percent)) + "%",
@@ -157,4 +150,4 @@ async def get_battery_health(**params: Any):
             "rating": rating,
         }
     except Exception as e:
-        print(e)
+        return {"error": f"An error occurred while fetching battery details: {e}"}
