fd7411352a484713aecb29c81b0eec6e874aa892
Muhammad_Butt <Muhammadmbutt1995@gmail.com>
2023-09-24 18:05:05 -0400

    init

--- tools/execution_functions/fminfo/get_trusted_network_status.py
+++ tools/execution_functions/fminfo/get_trusted_network_status.py
@@ -3,9 +3,11 @@ import time
 from typing import Any, Optional, Dict
 from pathlib import Path
 import plistlib
-
-# Import functions from user_env
-from utils.user_env import get_logged_in_username
+import winreg
+import os
+import win32security
+import win32profile
+from user_env import get_logged_in_username
 
 # Global status dictionary for connection states
 status = {
@@ -14,25 +16,27 @@ status = {
     "DISCONNECTED": "Please connect to either ZScaler ZPA or Citrix VPN to get onto a trusted network.",
 }
 
-# Conditionally import winreg if on Windows
-if platform.system() == "Windows":
-    import winreg
-
-
-# Windows-specific function to determine connection status
 async def windows_connection_status() -> Dict[str, Any]:
     RETRY_COUNT = 3
+    logged_in_user = get_logged_in_username()
 
     for _ in range(RETRY_COUNT):
         try:
-            # Connecting to the local registry
-            registry = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-            zscaler_key = winreg.OpenKey(registry, r"SOFTWARE\Zscaler\App")
+            # Get the SID for the logged-in user
+            user_sid, _, _ = win32security.LookupAccountName("", logged_in_user)
+            sid_string = win32security.ConvertSidToStringSid(user_sid)
+
+            # Connecting to the registry using the user's SID
+            registry = winreg.ConnectRegistry(None, winreg.HKEY_USERS)
+            zscaler_key_path = os.path.join(sid_string, r"SOFTWARE\Zscaler\App")
+            zscaler_key = winreg.OpenKey(registry, zscaler_key_path)
+            
             zpa_state, _ = winreg.QueryValueEx(zscaler_key, "ZPA_State")
             znw_state, _ = winreg.QueryValueEx(zscaler_key, "ZNW_State")
             winreg.CloseKey(zscaler_key)
             winreg.CloseKey(registry)
 
+            # Checking the Zscaler's state and returning the appropriate status
             if zpa_state == "TUNNEL_FORWARDING":
                 return {"status": "ZPA", "description": status["ZPA"], "rating": "ok"}
             elif znw_state == "TRUSTED":
@@ -44,7 +48,7 @@ async def windows_connection_status() -> Dict[str, Any]:
                     "rating": "warn",
                 }
 
-        except (FileNotFoundError, OSError, winreg.error):
+        except (FileNotFoundError, OSError, winreg.error, IndexError):
             continue
 
     return {
@@ -53,18 +57,15 @@ async def windows_connection_status() -> Dict[str, Any]:
         "rating": "warn",
     }
 
-
-# macOS-specific function to determine connection status
 async def macos_connection_status() -> Dict[str, Any]:
     RETRY_COUNT = 3
     SLEEP_TIME = 0.2
     log_dir = Path("/Library/Application Support/Zscaler")
-    logged_in_user = get_logged_in_username()
 
     for _ in range(RETRY_COUNT):
         try:
             # Reading the ztstatus log file
-            log_files = list(log_dir.glob(f"ztstatus_{logged_in_user}*.log"))
+            log_files = list(log_dir.glob("ztstatus_*.log"))
             if not log_files:
                 raise FileNotFoundError("No Zscaler log files found.")
 
@@ -94,8 +95,6 @@ async def macos_connection_status() -> Dict[str, Any]:
         "rating": "warn",
     }
 
-
-# Main function to determine the trusted network status based on the operating system
 async def get_trusted_network_status(
     params: Optional[Dict[str, Any]] = None
 ) -> Dict[str, Any]:
